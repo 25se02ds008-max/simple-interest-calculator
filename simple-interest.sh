3f25ee4d6dddf959dbdf83543d4fbb792bc1b5d1
25se02ds008-max <25se02ds008@ppsu.ac.in>
2026-08-16 13:15:21 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Simple Interest Calculator
+
 echo "Simple Interest Calculator"
 
-read -p "Enter Principal: " principal
+read -p "Enter Principal Amount: " principal
 read -p "Enter Rate of Interest: " rate
-read -p "Enter Time Period: " time
+read -p "Enter Time Period (in years): " time
 
 simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
 
+echo "Principal Amount: $principal"
+echo "Rate of Interest: $rate%"
+echo "Time Period: $time years"
 echo "Simple Interest: $simple_interest"
